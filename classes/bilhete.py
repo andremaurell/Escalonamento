ed75d04e2869086bb04ed1d78a1fe8d31aff6346
Gabriel Jaeger <23436587+gabrieltjaeger@users.noreply.github.com>
2022-06-13 18:44:43 -0300

    Create bilhete.py

---
+++ classes/bilhete.py
@@ -0,0 +1,11 @@
+from random import randint
+
+class Bilhete:
+    total = 0
+    dicionario_processos = {}
+    bilhetes_de_processos_que_acabaram = []
+    # implementar resorteamento de bilhetes de processos que já acabaram
+    
+    def sortear():
+        return randint(0, Bilhete.total-1)
+    
